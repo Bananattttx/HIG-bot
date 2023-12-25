ba93d114981446bcff900b8e17fdb84cd2bdc89d
Bananattttx <143480965+Bananattttx@users.noreply.github.com>
2023-12-25 13:14:01 +0000

    h

--- HIG-bot - GitHub.py
+++ HIG-bot - GitHub.py
@@ -1,15 +1,18 @@
 import praw
 
+modelh = ["h", "ℋ", "*h*", "**h**", "~~literally anything else~~ h", "h is awesome :]", "h :]", "# h", "# **h**"]
+
 reddit = praw.Reddit(
     client_id="###",
     client_secret="###",
     user_agent="HIG-bot by u/BananattttxNew",
-    username="###",
+    username="HIG-bot",
     password="###"
 )
 
 subreddit = reddit.subreddit("TheLetterH")
 
 for comment in subreddit.stream.comments(skip_existing=True):
-    if comment.author and comment.author.name == "h-bot-model-h" and comment.body == "h":
+    if comment.author and comment.author.name == "h-bot-model-h" and comment.body in modelh:
         comment.reply("H is gud")
+        print("new comment")
